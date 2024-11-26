b015c3736f9d917837dafb61d3231fe7aeb7b666
Mac02 <mac02@Mac02s-MacBook-Pro.local>
2024-11-26 15:51:35 +0530

    Uc2 to check whether two line are equal

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -4,6 +4,19 @@ import java.util.Scanner;
 
 public class LineComparison {
     public static void main(String[] args) {
+        Double lengthOfLine1 = getLengthOfLine();
+        System.out.println("Length of line 1 is " + lengthOfLine1);
+
+        Double lengthOfLine2 = getLengthOfLine();
+        System.out.println("Length of line 2 is " + lengthOfLine2);
+
+       if (lengthOfLine1.equals(lengthOfLine2)){
+           System.out.println("Two lines are equal");
+       }else {
+           System.out.println("Two lines are not equal");
+       }
+    }
+    private static double getLengthOfLine() {
         System.out.println("Enter x1 coordinate point and y1");
         Scanner sc = new Scanner(System.in);
         int x1 = sc.nextInt();
@@ -12,8 +25,9 @@ public class LineComparison {
         int x2 = sc.nextInt();
         int y2 = sc.nextInt();
 
-        System.out.println("x1--> "+x1 + " y1--> "+y1 + " x2--> "+x2 + " y2--> "+ y2);
+        System.out.println("x1--> " + x1 + " y1--> " + y1 + " x2--> " + x2 + " y2--> " + y2);
         double lengthOfLine = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
         System.out.println("length of a line is = " + lengthOfLine);
+        return lengthOfLine;
     }
 }
